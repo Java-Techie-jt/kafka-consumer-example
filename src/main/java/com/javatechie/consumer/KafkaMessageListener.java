66549049f9e51378a094f78cad954f6976e024a8
Basant kumar Hota <basahota@users.noreply.github.com>
2023-07-28 15:58:02 +0530

    kafka serialize and deserialize code

--- src/main/java/com/javatechie/consumer/KafkaMessageListener.java
+++ src/main/java/com/javatechie/consumer/KafkaMessageListener.java
@@ -1,5 +1,6 @@
 package com.javatechie.consumer;
 
+import com.javatechie.dto.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,23 +11,23 @@ public class KafkaMessageListener {
 
     Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
 
-    @KafkaListener(topics = "javatechie-demo2",groupId = "jt-group-new")
-    public void consume1(String message) {
-        log.info("consumer1 consume the message {} ", message);
+    @KafkaListener(topics = "javatechie-demo",groupId = "jt-group")
+    public void consumeEvents(Customer customer) {
+        log.info("consumer consume the events {} ", customer.toString());
     }
 
-    @KafkaListener(topics = "javatechie-demo1",groupId = "jt-group-new")
-    public void consume2(String message) {
-        log.info("consumer2 consume the message {} ", message);
-    }
-
-    @KafkaListener(topics = "javatechie-demo1",groupId = "jt-group-new")
-    public void consume3(String message) {
-        log.info("consumer3 consume the message {} ", message);
-    }
-
-    @KafkaListener(topics = "javatechie-demo1",groupId = "jt-group-new")
-    public void consume4(String message) {
-        log.info("consumer4 consume the message {} ", message);
-    }
+//    @KafkaListener(topics = "javatechie-demo1",groupId = "jt-group-new")
+//    public void consume2(String message) {
+//        log.info("consumer2 consume the message {} ", message);
+//    }
+//
+//    @KafkaListener(topics = "javatechie-demo1",groupId = "jt-group-new")
+//    public void consume3(String message) {
+//        log.info("consumer3 consume the message {} ", message);
+//    }
+//
+//    @KafkaListener(topics = "javatechie-demo1",groupId = "jt-group-new")
+//    public void consume4(String message) {
+//        log.info("consumer4 consume the message {} ", message);
+//    }
 }
